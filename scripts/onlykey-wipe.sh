73f3e2ddaa058e1eb66d0e7f739ec07073f82d83
echjansen <echjansen@outlook.com>
2024-06-07 11:05:06 +1000

    Added 'onlykey-wipe.sh' - Clear all ECC and RSA registers on OnlyKey.

---
+++ scripts/onlykey-wipe.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+onlykey-cli wipekey ECC1
+onlykey-cli setkey ECC1 label ""
+onlykey-cli wipekey ECC2
+onlykey-cli setkey ECC2 label ""
+onlykey-cli wipekey ECC3
+onlykey-cli setkey ECC3 label ""
+onlykey-cli wipekey ECC4
+onlykey-cli setkey ECC4 label ""
+onlykey-cli wipekey ECC5
+onlykey-cli setkey ECC5 label ""
+onlykey-cli wipekey ECC6
+onlykey-cli setkey ECC6 label ""
+onlykey-cli wipekey ECC7
+onlykey-cli setkey ECC7 label ""
+onlykey-cli wipekey ECC8
+onlykey-cli setkey ECC8 label ""
+onlykey-cli wipekey ECC9
+onlykey-cli setkey ECC9 label ""
+onlykey-cli wipekey ECC10
+onlykey-cli setkey ECC10 label ""
+onlykey-cli wipekey ECC11
+onlykey-cli setkey ECC11 label ""
+onlykey-cli wipekey ECC12
+onlykey-cli setkey ECC12 label ""
+onlykey-cli wipekey ECC13
+onlykey-cli setkey ECC13 label ""
+onlykey-cli wipekey ECC14
+onlykey-cli setkey ECC14 label ""
+onlykey-cli wipekey ECC15
+onlykey-cli setkey ECC15 label ""
+onlykey-cli wipekey ECC16
+onlykey-cli setkey ECC16 label ""
+
+onlykey-cli wipekey RSA1
+onlykey-cli setkey RSA1 label ""
+onlykey-cli wipekey RSA2
+onlykey-cli setkey RSA2 label ""
+onlykey-cli wipekey RSA3
+onlykey-cli setkey RSA3 label ""
+onlykey-cli wipekey RSA4
+onlykey-cli setkey RSA4 label ""
